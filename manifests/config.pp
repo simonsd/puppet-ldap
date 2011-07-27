9c8afac97ef14c8cff125700d7ae0f911d3b7fd3
Dave Simons <dave@inuits.be>
2011-07-27 09:08:31 +0200

    add some more config file templates remove exec for olcRootPW
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/config.pp
+++ manifests/config.pp
@@ -15,11 +15,51 @@ class ldap::config {
 			group => root,
 			mode => 0600,
 			content => template('ldap/bdb.ldif.erb');
-	}
 
-	exec {
-		'slappasswd':
-			command => "slappasswd -s $ldap_rootpass",
-			path => '/bin:/sbin:/usr/bin:/usr/sbin',
+		'monitor.ldif':
+			ensure => present,
+			path => '/etc/openldap/slapd.d/cn=config/olcDatabase={2}monitor.ldif',
+			owner => root,
+			group => root,
+			mode => 0600,
+			content => template('ldap/monitor.ldif.erb');
+
+		'/var/lib/ldap':
+			ensure => directory,
+			owner => ldap,
+			group => ldap,
+			recurse => true;
+
+		'db conf':
+			ensure => present,
+			path => '/var/lib/ldap/DB_CONFIG',
+			owner => ldap,
+			group => ldap,
+			mode => 0644,
+			content => template('ldap/db_config.erb');
+
+		'ldap prefs':
+			ensure => present,
+			path => '/etc/sysconfig/ldap',
+			owner => root,
+			group => root,
+			mode => 0644,
+			content => template('ldap/ldap.sysconf.erb');
+
+		'slapd cert':
+			ensure => present,
+			path => '/etc/pki/tls/certs/slapdcert.pem',
+			owner => root,
+			group => ldap,
+			mode => 0600,
+			content => template('ldap/slapdcert.pem.erb');
+
+		'slapd key':
+			ensure => present,
+			path => '/etc/pki/tls/certs/slapdkey.pem',
+			owner => root,
+			group => root,
+			mode => 0750,
+			content => template('ldap/slapdkey.pem.erb');
 	}
 }
